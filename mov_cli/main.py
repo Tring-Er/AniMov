426816245a08f5d4ae16857263e8bbef9d05051f
eXpansiiVe <sergio120800@gmail.com>
2022-11-28 11:13:50 +0100

    Changed import names

--- mov_cli/main.py
+++ mov_cli/main.py
@@ -2,32 +2,32 @@ import os
 import platform
 
 from mov_cli.utils.scraper import WebScraper
-from mov_cli.websites.theflix import Theflix
-from mov_cli.websites.vidsrc import Vidsrc
-from mov_cli.websites.eja import eja
+from mov_cli.websites.theflix import TheFlix
+from mov_cli.websites.vidsrc import VidSrc
+from mov_cli.websites.eja import Eja
 from mov_cli.websites.ask4movie import Ask4Movie
-from mov_cli.websites.ustvgo import ustvgo
-from mov_cli.websites.kimcartoon import kimcartoon
+from mov_cli.websites.ustvgo import Ustvgo
+from mov_cli.websites.kimcartoon import KimCartoon
 from mov_cli.websites.actvid import Actvid
 from mov_cli.websites.dopebox import DopeBox
 from mov_cli.websites.sflix import Sflix
 from mov_cli.websites.solar import Solar
-from mov_cli.websites.goal import goal9
+from mov_cli.websites.goal import Goal9
 from mov_cli.utils.onstartup import startup
 
 
 calls = {
-    "theflix": [Theflix, "https://theflix.to"],
-    "vidsrc": [Vidsrc, "https://v2.vidsrc.me"],
-    "eja": [eja, "https://eja.tv"],
+    "theflix": [TheFlix, "https://theflix.to"],
+    "vidsrc": [VidSrc, "https://v2.vidsrc.me"],
+    "eja": [Eja, "https://eja.tv"],
     "ask4movie": [Ask4Movie, "https://ask4movie.mx"],
-    "ustvgo": [ustvgo, "https://ustvgo.tv"],
-    "kimcartoon": [kimcartoon, "https://kimcartoon.li"],
+    "ustvgo": [Ustvgo, "https://ustvgo.tv"],
+    "kimcartoon": [KimCartoon, "https://kimcartoon.li"],
     "actvid": [Actvid, "https://www.actvid.com"],
     "sflix": [Sflix, "https://sflix.se"],
     "solar": [Solar, "https://solarmovie.pe"],
     "dopebox": [DopeBox, "https://dopebox.to"],
-    "9goal": [goal9, "https://9goal.tv/"],
+    "9goal": [Goal9, "https://9goal.tv/"],
 }
 
 
